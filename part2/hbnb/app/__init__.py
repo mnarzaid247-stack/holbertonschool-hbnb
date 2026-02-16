2b515498c5f4b9443312b1e77f30e73f44c0013b
Mis-hero <12423@holbertonstudents.com>
2026-02-17 01:56:27 +0300

    Refactor code structure for improved readability and maintainability

---
+++ part2/hbnb/app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask_restx import Api
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='app/v1/' )
