8df12dc45915eed1af75246378dd4a24bb703d54
manar <mnarzaid247@gmail.com>
2026-02-17 11:01:15 +0000

    doing the __init__ file for the app

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     app = Flask(__name__)
@@ -12,4 +13,5 @@ def create_app():
         doc='app/v1/'
     )
     api.add_namespace(users_ns, path="/api/v1/users")
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
     return app
