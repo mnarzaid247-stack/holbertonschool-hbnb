c4a707904f1388ed43e98207eb68c0fea3b0250a
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-02-20 20:26:29 +0300

    Clarify error message for user creation
    Updated error message for user creation to clarify input validation.

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -12,8 +12,11 @@ user_model = api.model(
         "email": fields.String(required=True, description="Email of the user"),
     },
 )
+
+
 @api.route("/")
 class UserList(Resource):
+
     @api.response(200, "Users retrieved successfully")
     def get(self):
         users = facade.get_all_users()
@@ -29,7 +32,7 @@ class UserList(Resource):
 
     @api.expect(user_model, validate=True)
     @api.response(201, "User successfully created")
-    @api.response(400, "Email already registered")
+    @api.response(400, "Invalid input data or email already registered")
     def post(self):
         user_data = api.payload
 
@@ -52,12 +55,14 @@ class UserList(Resource):
 
 @api.route("/<user_id>")
 class UserResource(Resource):
+
     @api.response(200, "User details retrieved successfully")
     @api.response(404, "User not found")
     def get(self, user_id):
         user = facade.get_user(user_id)
         if not user:
             return {"error": "User not found"}, 404
+
         return {
             "id": user.id,
             "first_name": user.first_name,
