7b44df0ec813f614e60f11c2b00c08314ee6ed46
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-03-01 14:15:25 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -9,7 +9,7 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from flask_bcrypt import Bcrypt
+
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
