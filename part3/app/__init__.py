0d813faf28658e86fa95ba4162683a1c643b724e
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-03-01 14:11:13 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt 
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
-
+jwt = JWTManager()
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -13,6 +14,7 @@ from flask_bcrypt import Bcrypt
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    jwt.init_app(app)
     bcrypt.init_app(app)
     api = Api(
             app,
