afd16d5a3a3e121ec6524d59bc3fcc491d89daf7
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-03-01 14:39:39 +0300

    Update user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import re
 from app.models.base_model import BaseModel
-from app import bcrypt
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, password=None, is_admin=False):
@@ -18,10 +17,12 @@ class User(BaseModel):
 
     def hash_password(self, password):
         """Hashes the password before storing it."""
+        from app import bcrypt
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def verify_password(self, password):
         """Verifies if the provided password matches the hashed password."""
+        from app import bcrypt
         return bcrypt.check_password_hash(self.password, password)
 
     def validate(self):
