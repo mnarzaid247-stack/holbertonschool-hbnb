5183b4d77dfbf6077c83ca923a0aa4286cd26e0c
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-03-01 14:21:48 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt 
 from flask_jwt_extended import JWTManager
+from app.api.v1.auth import api as auth_ns
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -27,4 +28,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    api.add_namespace(auth_ns, path="/api/v1")
     return app
