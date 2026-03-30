0ad9ffeed63b5717b44ba170a35022c769a72bc1
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-03-30 05:15:02 +0300

    Update seed.sql

---
+++ part3/sql/seed.sql
@@ -0,0 +1,16 @@
+USE hbnb_db;
+
+INSERT INTO user (id, first_name, last_name, email, password, is_admin)
+VALUES (
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'Admin',
+    'HBnB',
+    'admin@hbnb.io',
+    '$2b$12$REPLACE_WITH_REAL_BCRYPT_HASH',
+    TRUE
+);
+
+INSERT INTO amenity (id, name) VALUES
+('11111111-1111-1111-1111-111111111111', 'WiFi'),
+('22222222-2222-2222-2222-222222222222', 'Swimming Pool'),
+('33333333-3333-3333-3333-333333333333', 'Air Conditioning');
