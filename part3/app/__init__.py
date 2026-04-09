7c1050c5ca66ece21adce1900df3ebe3ecc44bcd
manar <mnarzaid247@gmail.com>
2026-04-09 16:11:21 +0300

    CORS

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -11,6 +12,7 @@ jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config_class)
 
     db.init_app(app)
