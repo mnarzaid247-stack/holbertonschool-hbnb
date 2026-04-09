68411d9a592fa14265b34d0616f7fc684ec2a92e
manar <mnarzaid247@gmail.com>
2026-04-09 16:41:45 +0300

    update

--- part4/scripts.js
+++ part4/scripts.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+
   const loginForm = document.getElementById('login-form');
 
   if (loginForm) {
@@ -25,4 +26,82 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   }
+
+  const loginLink = document.getElementById("login-link");
+  const token = getCookie("token");
+
+  if (loginLink) {
+    if (token) {
+      loginLink.style.display = "none";
+    } else {
+      loginLink.style.display = "inline";
+    }
+  }
+
+  const placesList = document.getElementById("places-list");
+
+  if (placesList) {
+    try {
+      const places = await fetchPlaces();
+      displayPlaces(places);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 });
+
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].split("=");
+
+    if (cookie[0] === name) {
+      return cookie[1];
+    }
+  }
+
+  return null;
+}
+
+async function fetchPlaces() {
+  const token = getCookie("token");
+
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  const response = await fetch("http://127.0.0.1:5000/api/v1/places", {
+    method: "GET",
+    headers: headers
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch places");
+  }
+
+  const places = await response.json();
+  return places;
+}
+
+function displayPlaces(places) {
+  const placesList = document.getElementById("places-list");
+  placesList.innerHTML = "";
+
+  places.forEach((place) => {
+    const placeCard = document.createElement("div");
+    placeCard.className = "place-card";
+
+    placeCard.innerHTML = `
+      <h3>${place.title}</h3>
+      <p>Price: ${place.price} $/night</p>
+      <button class="details-button">View Details</button>
+    `;
+
+    placesList.appendChild(placeCard);
+  });
+}
