ed416d5d2634adfb0dbc7753c2587de792b03730
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-04-09 15:16:13 +0300

    update

---
+++ part4/scripts.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        document.cookie = `token=${data.access_token}; path=/`;
+        window.location.href = 'index.html';
+      } else {
+        alert('Login failed: ' + response.statusText);
+      }
+    });
+  }
+});
