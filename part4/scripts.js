c665f73d7195381f978dd9d221ff8ff71d2cf09b
manar <mnarzaid247@gmail.com>
2026-04-09 16:23:00 +0300

    update

--- part4/scripts.js
+++ part4/scripts.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const email = document.getElementById('email').value;
       const password = document.getElementById('password').value;
 
-      const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+      const response = await fetch('http://127.0.0.1:5000/api/v1/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
